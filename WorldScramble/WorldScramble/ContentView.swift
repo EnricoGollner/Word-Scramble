8dbdb626ef136cfe6346d66c0cd519dd38e3b758
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-11-28 19:51:38 -0300

    Done - V1.0

--- WorldScramble/WorldScramble/ContentView.swift
+++ WorldScramble/WorldScramble/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var usedWords = [String]()
+    @State private var newWord =  ""
     @State private var rootWord = ""
-    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View{
         NavigationStack{
@@ -30,33 +34,84 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
-            .onAppear(perform: startGame)  // call the func when the view is shown
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError){
+                Button("OK", role: .cancel){  }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
     func addNewWord(){
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        guard answer.count > 0 else { return }
+        // Verifications:
+        guard answer.count > 0 else{ return }
+        
+        guard isOriginal(word: answer) else{
+            wordError(title: "Word already used", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else{
+            wordError(title: "Word not possible", message: "You can't spell that word from \(rootWord)!")
+            return
+        }
+        
+        guard isReal(word: answer) else{
+            wordError(title: "Word not recognize", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation{
-            usedWords.insert(newWord, at: 0)
+            usedWords.insert(answer, at: 0)
         }
         
         newWord = ""
     }
     
     func startGame(){
-        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
-            if let startWords = try? String(contentsOf: startWordsUrl){
-                let allWords = startWords.components(separatedBy: "\n")
+        if let startUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startFile = try? String(contentsOf: startUrl){
+                let allWords = startFile.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
-                return  // only to exit
+            }
+        }
+    }
+    
+    func isOriginal(word: String) -> Bool{
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool{
+        var tempWord = rootWord
+        
+        for letter in word{
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            } else{
+                return false
             }
         }
         
-        fatalError("Could not load start.txt from bundle.")
+        return true
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
         
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
